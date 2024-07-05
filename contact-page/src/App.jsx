528db3d18c8d06ccb2fcfe1075b0e18bf5e241ad
Ukasha <116743795+Ukashashere@users.noreply.github.com>
2024-07-06 04:13:05 +0530

    commit 4

--- contact-page/src/App.jsx
+++ contact-page/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Navigation from './components/navigation/navigation'
+import ContactHeader from './components/ContactHeader/ContactHeader'
 
 function App() {
   return(
     <div>
       <Navigation />
+      <ContactHeader />
     </div>
   );
 }

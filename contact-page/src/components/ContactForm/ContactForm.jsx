42e993e05169afe716963c1acb3b032a51c82675
Ukasha <116743795+Ukashashere@users.noreply.github.com>
2024-07-07 17:10:27 +0530

    commit 7

--- contact-page/src/components/ContactForm/ContactForm.jsx
+++ contact-page/src/components/ContactForm/ContactForm.jsx
@@ -7,8 +7,10 @@ const ContactForm = () => {
   return (
     <section className={styles.container}>
         <div className={styles.contact_form}>
-          <Button text="VIA SUPPORT CHAT" icon={<MdMessage fontSize="24px" />} />
-          <Button text="VIA CALL" icon={<FaPhoneAlt fontSize="24px" />} />
+          <div className={styles.top_btn}>
+            <Button text="VIA SUPPORT CHAT" icon={<MdMessage fontSize="24px" />} />
+            <Button text="VIA CALL" icon={<FaPhoneAlt fontSize="24px" />} />
+          </div>          
         </div>  
         <div className={styles.contact_image}></div>
     </section>
